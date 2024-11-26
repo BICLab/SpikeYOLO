4fef9c3eddce397f772f1a994c2f35e62a457e56
XinhaoLuo666 <1796415836@qq.com>
2024-11-26 13:02:02 +0800

    update yaml

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ print(model)
 
 #train
 # model.train(data="coco.yaml",device=[7],epochs=100)  # train the model
-model.train(data="coco.yaml",device=[4,5],epochs=100)  # train the model
+model.train(data="coco.yaml",device=[4],epochs=100)  # train the model
 
 #TEST
 # model = YOLO('runs/detect/train1/weights/last.pt')  # load a pretrained model (recommended for training)
